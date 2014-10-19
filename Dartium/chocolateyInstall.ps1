215b437152cd59189d410030a502f08a86333cbd
Danny Tuppeny <danny@tuppeny.com>
2014-10-19 13:48:25 +0100

    Added first cut of Dartium package.

---
+++ Dartium/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+$packageName = 'dartium'
+
+# https://www.dartlang.org/tools/download_archive/
+$url = 'https://storage.googleapis.com/dart-archive/channels/stable/release/41096/dartium/dartium-windows-ia32-release.zip'
+
+$binRoot = Get-BinRoot
+$installDir = Join-Path $binRoot "dartium"
+
+if (test-path $installDir) {
+	Remove-Item $installDir -Recurse -Force
+}
+
+Install-ChocolateyZipPackage $packageName $url $installDir
+
+# Dartium is zipped inside a folder, so we want to move everything up a level
+if ((dir $installDir | measure-object).Count -eq 1) {
+	$dartiumSubfolder = (dir $installDir).FullName
+	dir $dartiumSubfolder | move-item -Destination $installDir
+	remove-item $dartiumSubfolder -Force
+}
