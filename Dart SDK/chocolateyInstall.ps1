b3278b5bbbb460da5958e272689b19e6e5a13d36
daftspaniel <daftspaniel@gmail.com>
2017-01-05 20:00:31 +0000

    Added checksums.

--- Dart SDK/chocolateyInstall.ps1
+++ Dart SDK/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $packageName = 'dart-sdk'
 $url = 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.21.0/sdk/dartsdk-windows-ia32-release.zip'
 $url64 = 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.21.0/sdk/dartsdk-windows-x64-release.zip'
 
-$unzipDir = Get-BinRoot # NOTE: We're not adding dart-sdk to the end, as that's already in the zip file.
-$installDir = Join-Path $unzipDir "dart-sdk"
+$unzipLocation = Get-BinRoot
+$installDir = Join-Path $unzipLocation "dart-sdk"
 
 Install-ChocolateyPath "$installDir\bin"
 Install-ChocolateyPath "${env:USERPROFILE}\AppData\Roaming\Pub\Cache\bin"
@@ -13,4 +13,16 @@ $env:Path = "$($env:Path);$installDir\bin;${env:USERPROFILE}\AppData\Roaming\Pub
 if (test-path $installDir) {
 	Remove-Item $installDir -Recurse -Force
 }
-Install-ChocolateyZipPackage $packageName $url $unzipDir $url64
+
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url
+  url64bit       = $url64
+  unzipLocation  = $unzipLocation
+  checksum       = '260a5b0abf4e361ed1d8a9a8371767edffbcba28c9f00d9a85bff87f1fe9d7d3'
+  checksumType   = 'sha256'
+  checksum64     = '1b877c7dd8110b841b778270e82e7421b9d0def16987ed6cfcbf8d40393d6134'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
